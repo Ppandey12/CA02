24fd324cf4ed3d85b374f11f1b93e9a6ab53d336
Ppandey12 <paraspandey827@gmail.com>
2023-05-03 13:37:12 -0400

    fixed

--- firstapp/models/Openai_models.js
+++ firstapp/models/Openai_models.js
@@ -1,15 +1,15 @@
 
-'use strict';
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const ObjectId = mongoose.Schema.Types.ObjectId;
+// 'use strict';
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const transSchema = Schema({
-  description: String,
-  category: String,
-  amount: Number,
-  date: String,
-  userId: { type: ObjectId, ref: 'user' }
-});
+// const OpenaiSchema = Schema({
+//   description: String,
+//   // category: String,
+//   // amount: Number,
+//   // date: String,
+//   userId: { type: ObjectId, ref: 'user' }
+// });
 
-module.exports = mongoose.model('transactionItem', transSchema);
+// module.exports = mongoose.model('Openai_models', OpenaiSchema);
